c5ebdc414fce5864bf8604ee00565822c71cc639
John Anderson <jander43@vols.utk.edu>
2017-04-15 18:21:03 -0400

    Initial commit

---
+++ meda.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# globals
+
+# funcs
+walk_fs() {
+  # recursively walk file system
+  # TODO
+  :
+}
+
+init() {
+  # check if exists
+  # TODO
+
+  # create
+  mkdir '.meda' || exit
+}
