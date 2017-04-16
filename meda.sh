925712c2be733e2c04030732367860cb8b611275
John Anderson <jander43@vols.utk.edu>
2017-04-15 20:15:05 -0400

    Updated init implementation

--- meda.sh
+++ meda.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # globals
+refs_dir='.meda/refs'
+content_dir='.meda/content'
 
 # funcs
 walk_fs() {
@@ -9,10 +11,36 @@ walk_fs() {
   :
 }
 
-init() {
-  # check if exists
+repo_exists(){
+  # check for .meda repo
+  # TODO
+  :
+}
+
+print_refs() {
+  # print out refs to files in project tree
+  # TODO
+  :
+}
+
+meda_init() {
+  # check if .meda repo exists
+  # TODO
+
+  # check if arg 2 is a dir
   # TODO
 
   # create
-  mkdir '.meda' || exit
+  mkdir -p "${refs_dir}" && mkdir -p "${content_dir}" || exit
 }
+
+
+# main interface
+if [ -z "$1" ]; then
+  print_refs
+else
+  case "$1" in
+    add) : ;;
+    init) meda_init "$2" ;;
+  esac
+fi
